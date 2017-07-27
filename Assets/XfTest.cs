73948f2e6ea059815cecf2c6fd11d7713410a21e
Tohka <1758200771@qq.coom>
2017-07-27 17:20:51 +0800

    Signed-off-by: Tohka <1758200771@qq.coom>

---
+++ Assets/XfTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XfTest : MonoBehaviour {
+
+    private string showResult = "";
+
+    void OnGUI()
+
+    {
+
+        if (GUILayout.Button("startRecognizer", GUILayout.Height(100)))
+        {
+
+            AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+
+            AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+
+            jo.Call("startSpeechListener");
+
+        }
+
+        GUILayout.TextArea(showResult, GUILayout.Width(200));
+
+    }
+
+
+
+    public void LogXfMessage(string recognizerResult)
+
+    {
+
+        showResult += recognizerResult;
+
+    }
+}
